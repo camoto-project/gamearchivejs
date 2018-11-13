f335a96f3b8c0633a2a8d88615f688ea6edf5934
Adam Nielsen <malvineous@shikadi.net>
2018-11-13 21:53:09 +1000

    Add Archive and File classes alongside ArchiveHandler

--- formats/archive.js
+++ formats/archive.js
@@ -1,25 +1,76 @@
-module.exports = class ArchiveHandler
-{
-	/// Identify any problems writing the given archive in the current format.
-	/**
-	 * @param Object archive
-	 *   Archive to attempt to write in this handler's format.
-	 *
-	 * @return Array of strings listing any problems that will prevent the
-	 *   supplied archive from being written in this format.  An empty array
-	 *   indicates no problems.
-	 */
-	static checkLimits(archive)
-	{
-		const { limits } = this.metadata();
-		let issues = [];
-		if (limits.maxFilenameLen) {
-			archive.files.forEach(file => {
-				if (file.name.length > limits.maxFilenameLen) {
-					issues.push(`Filename length is ${file.name.length}, max is ${limits.maxFilenameLen}: ${file.name}`);
-				}
-			});
-		}
-		return issues;
+module.exports = class Archive {
+	constructor() {
+		/// Any metadata describing the archive file itself goes here.
+		/**
+		 * Some archives have a description, comment, or other information that's
+		 * not otherwise part of any file inside the archive.
+		 */
+		this.metadata = {};
+
+		/// All the files in the archive.
+		/**
+		 * Each element in the array is an Archive.File object.
+		 */
+		this.files = [];
+	};
+};
+
+module.exports.File = class File {
+	constructor() {
+		// Filename.
+		this.name = null;
+
+		// File type if known, as "major/minor".  Major is typically the type of
+		// editor (map, image, music, etc.) and minor is the file format.  This
+		// should only be set if the information is provided by the archive
+		// metadata.
+		this.type = undefined;
+
+		/// On-disk file size.
+		/**
+		 * This is the number of bytes the file takes up inside the archive file,
+		 * not counting any file-specific header that is not part of the file
+		 * content.  If the file is compressed, this is the compressed size.  If the
+		 * file is not compresed, this will be the same as nativeSize.
+		 */
+		this.diskSize = undefined;
+
+		/// Native file size.
+		/**
+		 * This is the number of bytes in the file's native format, once extracted
+		 * from the archive.  If the file is compressed, this is the decompressed
+		 * size.  If the file is not compressed, this will be the same as diskSize.
+		 */
+		this.nativeSize = undefined;
+
+		// Default attributes for this file.
+		this.attributes = {
+			compressed: false,
+			encrypted: false,
+		};
+
+		this.getContent = () => this.getRaw();
 	}
-}
+
+	/// Read this file.
+	/**
+	 * By default the content returned is exactly the same as the raw data.
+	 * This function should be overridden if processing needs to be performed on
+	 * the raw data before it is suitable for use, e.g. by decompressing it.
+	 *
+	 * @return Buffer containing the file data in its native (decompressed,
+	 *   decrypted) format.
+	 */
+	getContent() {
+		return this.getRaw();
+	}
+
+	/// Read the file exactly as it is in the archive.
+	/**
+	 * This function will need to be overridden in every file, otherwise it will
+	 * not be possible to extract files from the archive.
+	 */
+	getRaw() {
+		throw new Error('getRaw() has not been supplied for this file!');
+	}
+};
---
+++ formats/archiveHandler.js
@@ -0,0 +1,102 @@
+module.exports = class ArchiveHandler
+{
+	/// Retrieve information about the archive file format.
+	/**
+	 * This must be overridden by all format handlers.  It returns a structure
+	 * detailed below.
+	 */
+	static metadata() {
+		return {
+			/// A unique identifier for the format.
+			id: 'unknown',
+
+			/// The user-friendly title for the format.
+			title: 'Unknown format',
+
+			/// A list of filename globs that match files in this format, if any.
+			/**
+			 * Examples might be ['*.txt', '*.doc', 'file*.bin'].
+			 */
+			glob: [],
+
+			/// Any limitations of the archive format.
+			limits: {
+				/// Number of characters in the filename, including dots.
+				/**
+				 * If the archive can only store normal DOS 8.3 filenames, then this
+				 * would be 12.  If omitted there is no restriction on filename length.
+				 */
+				maxFilenameLen: undefined,
+			},
+		};
+	}
+
+	/// Identify any problems writing the given archive in the current format.
+	/**
+	 * @param Object archive
+	 *   Archive to attempt to write in this handler's format.
+	 *
+	 * @return Array of strings listing any problems that will prevent the
+	 *   supplied archive from being written in this format.  An empty array
+	 *   indicates no problems.
+	 */
+	static checkLimits(archive)
+	{
+		const { limits } = this.metadata();
+		let issues = [];
+		if (limits.maxFilenameLen) {
+			archive.files.forEach(file => {
+				if (file.name.length > limits.maxFilenameLen) {
+					issues.push(`Filename length is ${file.name.length}, max is ${limits.maxFilenameLen}: ${file.name}`);
+				}
+			});
+		}
+		return issues;
+	}
+
+	/// See if the given archive is in the format supported by this handler.
+	/**
+	 * This is used for format autodetection.
+	 *
+	 * @note More than one handler might report that it supports a file format,
+	 *   such as the case of an empty file, which is a valid empty archive in a
+	 *   number of different file formats.
+	 *
+	 * @param Buffer content
+	 *   The archive to examine.
+	 *
+	 * @return true if the data is definitely in this format, false if it is
+	 *   definitely not in this format, and undefined if the data could not be
+	 *   positively identified but it's possible it is in this format.
+	 */
+	static identify(content) {
+		return false;
+	};
+
+	/// Read the given archive file.
+	/**
+	 * @param Buffer content
+	 *   File content of the archive.
+	 *
+	 * @return Archive object detailing the contents of the archive file.
+	 */
+	static parse(content) {
+		throw new Error('Not implemented yet.');
+	};
+
+	/// Write out an archive file in this format.
+	/**
+	 * @preconditions The archive has already been passed through checkLimits()
+	 *   successfully. If not, the behaviour is undefined and a corrupted file
+	 *   might be produced.
+	 *
+	 * @param Archive archive
+	 *   The contents of the file to write.
+	 *
+	 * @return Buffer instance containing the contents of the file, suitable for
+	 *   writing directly to a file on disk or offering for download to the user.
+	 */
+	static generate(archive) {
+		throw new Error('Not implemented yet.');
+	};
+};
