1913a4ae2d6e02788f2103d8485e0c378250bcb2
Adam Nielsen <malvineous@shikadi.net>
2018-11-17 23:10:30 +1000

    Add licence header to source files

--- formats/arc-rff-blood.js
+++ formats/arc-rff-blood.js
@@ -1,3 +1,25 @@
+/**
+ * @brief Blood .RFF format handler.
+ *
+ * This file format is fully documented on the ModdingWiki:
+ *   http://www.shikadi.net/moddingwiki/RFF_Format
+ *
+ * Copyright (C) 2018 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const { RecordBuffer, RecordType } = require('@malvineous/record-io-buffer');
 const GameCompression = require('@malvineous/gamecomp');
 
