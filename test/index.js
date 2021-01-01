96e9f079f7913f537189bf0cad5a1ea14946065f
Adam Nielsen <malvineous@shikadi.net>
2021-01-02 00:14:21 +1000

    Update more copyright years

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 /**
  * @file Standard tests.
  *
- * Copyright (C) 2018-2019 Adam Nielsen <malvineous@shikadi.net>
+ * Copyright (C) 2010-2021 Adam Nielsen <malvineous@shikadi.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
