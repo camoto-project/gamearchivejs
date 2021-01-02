4bbd33d326cda965e211950e89fe3e2466a47856
Adam Nielsen <malvineous@shikadi.net>
2021-01-02 17:48:54 +1000

    arc-wad-doom: Add identify() tests

--- test/test-arc-wad-doom.js
+++ test/test-arc-wad-doom.js
@@ -29,189 +29,232 @@ const handler = GameArchive.getHandler(format);
 const md = handler.metadata();
 let testutil = new TestUtil(md.id);
 describe(`Extra tests for ${md.title} [${md.id}]`, function() {
-
 	let content = {};
-	before('load test data from local filesystem', function() {
-		content = testutil.loadContent(handler, [
-			'folders',
-			'folders_map',
-		]);
-	});
 
-	describe('parse()', function() {
-		it('start/end entries converted into folders', function() {
-			let archive = handler.parse(content.folders);
-			assert.equal(archive.files[0].name.toUpperCase(), 'E1M1/THINGS');
-			assert.equal(archive.files[1].name.toUpperCase(), 'E1M1/BEHAVIOR');
-			assert.equal(archive.files[2].name.toUpperCase(), 'E1M2/LINEDEFS');
-			assert.equal(archive.files[3].name.toUpperCase(), 'E1M2/BLOCKMAP');
-			assert.equal(archive.files[4].name.toUpperCase(), 'ROOT');
-			assert.equal(archive.files[5].name.toUpperCase(), 'A/B/TEST');
-			assert.equal(archive.files[6].name.toUpperCase(), 'ROOT2');
-		});
-	});
+	describe('I/O', function() {
 
-	describe('generate()', function() {
-		it('folders are converted into start/end codes (ExMx)', async function() {
-			let archive = new Archive();
-
-			let file = new Archive.File();
-			file.name = 'E1M1/THINGS';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the first file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'E1M1/BEHAVIOR';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the second file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'E1M2/LINEDEFS';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the third file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'E1M2/BLOCKMAP';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the fourth file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'ROOT';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the first file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'A/B/TEST';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the second file');
-			archive.files.push(file);
-
-			file = new Archive.File();
-			file.name = 'ROOT2';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the third file');
-			archive.files.push(file);
-
-			const contentGenerated = handler.generate(archive);
-			TestUtil.contentEqual(content.folders, contentGenerated);
+		before('load test data from local filesystem', function() {
+			content = testutil.loadContent(handler, [
+				'short',
+				'wrong_sig',
+				'fat_past_eof',
+				'folders',
+				'folders_map',
+			]);
 		});
 
-		it('folders are converted into start/end codes (MAPxx)', async function() {
-			let archive = new Archive();
+		describe('identify()', function() {
 
-			let file = new Archive.File();
-			file.name = 'MAP01/THINGS';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the first file');
-			archive.files.push(file);
+			it('should reject short files', function() {
+				const result = handler.identify(
+					content['short'].main,
+					content['short'].main.filename
+				);
+				assert.equal(result.reason, 'Content too short (< 12 b).');
+				assert.equal(result.valid, false);
+			});
 
-			file = new Archive.File();
-			file.name = 'MAP01/BEHAVIOR';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the second file');
-			archive.files.push(file);
+			it('should reject wrong signature', function() {
+				const result = handler.identify(
+					content['wrong_sig'].main,
+					content['wrong_sig'].main.filename
+				);
+				assert.equal(result.reason, `Incorrect signature "FWAD".`);
+				assert.equal(result.valid, false);
+			});
 
-			file = new Archive.File();
-			file.name = 'MAP02/LINEDEFS';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the third file');
-			archive.files.push(file);
+			it('should reject FAT beyond EOF', function() {
+				const result = handler.identify(
+					content['fat_past_eof'].main,
+					content['fat_past_eof'].main.filename
+				);
+				assert.equal(result.reason, `FAT offset (172) is past the end of the file (166).`);
+				assert.equal(result.valid, false);
+			});
 
-			file = new Archive.File();
-			file.name = 'MAP02/BLOCKMAP';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the fourth file');
-			archive.files.push(file);
+		}); // identify()
 
-			file = new Archive.File();
-			file.name = 'ROOT';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the first file');
-			archive.files.push(file);
+		describe('parse()', function() {
 
-			file = new Archive.File();
-			file.name = 'A/B/TEST';
-			file.nativeSize = 23;
-			file.getRaw = () => TestUtil.u8FromString('This is the second file');
-			archive.files.push(file);
+			it('start/end entries converted into folders', function() {
+				let archive = handler.parse(content.folders);
+				assert.equal(archive.files[0].name.toUpperCase(), 'E1M1/THINGS');
+				assert.equal(archive.files[1].name.toUpperCase(), 'E1M1/BEHAVIOR');
+				assert.equal(archive.files[2].name.toUpperCase(), 'E1M2/LINEDEFS');
+				assert.equal(archive.files[3].name.toUpperCase(), 'E1M2/BLOCKMAP');
+				assert.equal(archive.files[4].name.toUpperCase(), 'ROOT');
+				assert.equal(archive.files[5].name.toUpperCase(), 'A/B/TEST');
+				assert.equal(archive.files[6].name.toUpperCase(), 'ROOT2');
+			});
 
-			file = new Archive.File();
-			file.name = 'ROOT2';
-			file.nativeSize = 22;
-			file.getRaw = () => TestUtil.u8FromString('This is the third file');
-			archive.files.push(file);
+		}); // parse()
 
-			const contentGenerated = handler.generate(archive);
-			TestUtil.contentEqual(content.folders_map, contentGenerated);
-		});
+		describe('generate()', function() {
 
-		it('maximum filename length is correct', function() {
-			let archive = new Archive();
-			let file = new Archive.File();
+			it('folders are converted into start/end codes (ExMx)', async function() {
+				let archive = new Archive();
 
-			let expectedName = 'AAAAAAAA';
+				let file = new Archive.File();
+				file.name = 'E1M1/THINGS';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the first file');
+				archive.files.push(file);
 
-			file.name = expectedName;
-			file.nativeSize = 16;
-			file.getRaw = () => TestUtil.u8FromString('longest filename');
-			archive.files.push(file);
+				file = new Archive.File();
+				file.name = 'E1M1/BEHAVIOR';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the second file');
+				archive.files.push(file);
 
-			const issues = handler.checkLimits(archive);
-			assert.equal(issues.length, 0, `${issues.length} issues with archive, expected 0`);
+				file = new Archive.File();
+				file.name = 'E1M2/LINEDEFS';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the third file');
+				archive.files.push(file);
 
-			const contentGenerated = handler.generate(archive);
+				file = new Archive.File();
+				file.name = 'E1M2/BLOCKMAP';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the fourth file');
+				archive.files.push(file);
 
-			const parsedArchive = handler.parse(contentGenerated);
-			assert.ok(parsedArchive.files, 'Incorrect archive returned');
-			assert.ok(parsedArchive.files[0], 'File did not get added to archive');
-			assert.equal(parsedArchive.files[0].name, expectedName, 'Name does not match');
+				file = new Archive.File();
+				file.name = 'ROOT';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the first file');
+				archive.files.push(file);
 
-			// Again, this time with a name that's too long.
-			archive = new Archive();
+				file = new Archive.File();
+				file.name = 'A/B/TEST';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the second file');
+				archive.files.push(file);
 
-			expectedName = 'AA/TESTTEST1';
+				file = new Archive.File();
+				file.name = 'ROOT2';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the third file');
+				archive.files.push(file);
 
-			file.name = expectedName;
-			archive.files.push(file);
-			const issues1 = handler.checkLimits(archive);
-			assert.equal(issues1.length, 1, `${issues1.length} issues with archive, expected 1`);
-		});
+				const contentGenerated = handler.generate(archive);
+				TestUtil.contentEqual(content.folders, contentGenerated);
+			});
 
-		it('maximum folder name length is correct', function() {
-			let archive = new Archive();
-			let file = new Archive.File();
+			it('folders are converted into start/end codes (MAPxx)', async function() {
+				let archive = new Archive();
 
-			let expectedName = 'AA/TEST';
+				let file = new Archive.File();
+				file.name = 'MAP01/THINGS';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the first file');
+				archive.files.push(file);
 
-			file.name = expectedName;
-			file.nativeSize = 16;
-			file.getRaw = () => TestUtil.u8FromString('longest filename');
-			archive.files.push(file);
+				file = new Archive.File();
+				file.name = 'MAP01/BEHAVIOR';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the second file');
+				archive.files.push(file);
 
-			const issues = handler.checkLimits(archive);
-			assert.equal(issues.length, 0, `${issues.length} issues with archive, expected 0`);
+				file = new Archive.File();
+				file.name = 'MAP02/LINEDEFS';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the third file');
+				archive.files.push(file);
 
-			const contentGenerated = handler.generate(archive);
+				file = new Archive.File();
+				file.name = 'MAP02/BLOCKMAP';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the fourth file');
+				archive.files.push(file);
 
-			const parsedArchive = handler.parse(contentGenerated);
-			assert.ok(parsedArchive.files, 'Incorrect archive returned');
-			assert.ok(parsedArchive.files[0], 'File did not get added to archive');
-			assert.equal(parsedArchive.files[0].name, expectedName, 'Name does not match');
+				file = new Archive.File();
+				file.name = 'ROOT';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the first file');
+				archive.files.push(file);
 
-			// Again, this time with a name that's too long.
-			archive = new Archive();
+				file = new Archive.File();
+				file.name = 'A/B/TEST';
+				file.nativeSize = 23;
+				file.getRaw = () => TestUtil.u8FromString('This is the second file');
+				archive.files.push(file);
 
-			expectedName = 'AAA/TESTTEST';
+				file = new Archive.File();
+				file.name = 'ROOT2';
+				file.nativeSize = 22;
+				file.getRaw = () => TestUtil.u8FromString('This is the third file');
+				archive.files.push(file);
 
-			file.name = expectedName;
-			archive.files.push(file);
-			const issues1 = handler.checkLimits(archive);
-			assert.equal(issues1.length, 1, `${issues1.length} issues with archive, expected 1`);
-		});
-	});
-});
+				const contentGenerated = handler.generate(archive);
+				TestUtil.contentEqual(content.folders_map, contentGenerated);
+			});
+
+			it('maximum filename length is correct', function() {
+				let archive = new Archive();
+				let file = new Archive.File();
+
+				let expectedName = 'AAAAAAAA';
+
+				file.name = expectedName;
+				file.nativeSize = 16;
+				file.getRaw = () => TestUtil.u8FromString('longest filename');
+				archive.files.push(file);
+
+				const issues = handler.checkLimits(archive);
+				assert.equal(issues.length, 0, `${issues.length} issues with archive, expected 0`);
+
+				const contentGenerated = handler.generate(archive);
+
+				const parsedArchive = handler.parse(contentGenerated);
+				assert.ok(parsedArchive.files, 'Incorrect archive returned');
+				assert.ok(parsedArchive.files[0], 'File did not get added to archive');
+				assert.equal(parsedArchive.files[0].name, expectedName, 'Name does not match');
+
+				// Again, this time with a name that's too long.
+				archive = new Archive();
+
+				expectedName = 'AA/TESTTEST1';
+
+				file.name = expectedName;
+				archive.files.push(file);
+				const issues1 = handler.checkLimits(archive);
+				assert.equal(issues1.length, 1, `${issues1.length} issues with archive, expected 1`);
+			});
+
+			it('maximum folder name length is correct', function() {
+				let archive = new Archive();
+				let file = new Archive.File();
+
+				let expectedName = 'AA/TEST';
+
+				file.name = expectedName;
+				file.nativeSize = 16;
+				file.getRaw = () => TestUtil.u8FromString('longest filename');
+				archive.files.push(file);
+
+				const issues = handler.checkLimits(archive);
+				assert.equal(issues.length, 0, `${issues.length} issues with archive, expected 0`);
+
+				const contentGenerated = handler.generate(archive);
+
+				const parsedArchive = handler.parse(contentGenerated);
+				assert.ok(parsedArchive.files, 'Incorrect archive returned');
+				assert.ok(parsedArchive.files[0], 'File did not get added to archive');
+				assert.equal(parsedArchive.files[0].name, expectedName, 'Name does not match');
+
+				// Again, this time with a name that's too long.
+				archive = new Archive();
+
+				expectedName = 'AAA/TESTTEST';
+
+				file.name = expectedName;
+				archive.files.push(file);
+				const issues1 = handler.checkLimits(archive);
+				assert.equal(issues1.length, 1, `${issues1.length} issues with archive, expected 1`);
+			});
+
+		}); // generate()
+
+	}); // I/O
+
+}); // Extra tests
